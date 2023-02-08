1d143eadad956a8741b7c95e6bfaa0567ed484b6
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-02-08 12:01:30 +0100

    some tests

---
+++ Docker_tests/app.js
@@ -0,0 +1,8 @@
+console.log("hello souchen");
+
+// so to run this app we need write this instructions inside a dockerFile and let docker package our application 
+
+//1: start with an os
+//2: install node
+//3: copy app files
+//4: run node app.js
